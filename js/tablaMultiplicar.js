aa21dc6ac280e2289ea38ac322aba3714fce9d1f
decunez <decunez@espe.edu.ec>
2025-05-22 20:39:36 -0500

    1

--- js/tablaMultiplicar.js
+++ js/tablaMultiplicar.js
@@ -1,10 +1,11 @@
 let numero = parseInt(prompt("Ingresa un número para ver su tabla de multiplicar:"));
+
 console.log("Actividad 2: FOR - Tabla de multiplicar");
 
 if (!isNaN(numero)) {
     console.log("Tabla de muliplicar del "+numero+"");
     for (let i = 1; i <= 12; i++) {
-        console.log(`${numero} x ${i} = ${numero * i}`);
+        console.log(i  + " X " + numero + " =" + i*numero)
     }
 } else {
     console.log("Por favor ingresa un número válido.");
--- js/tablasParesImpares.js
+++ js/tablasParesImpares.js
@@ -2,15 +2,21 @@ let opcion = prompt("¿Quieres ver las tablas de multiplicar de números pares o
 console.log("Actividad 3: IF + FOR - Tablas pares o impares");
 
 if (opcion === "pares" || opcion === "impares") {
-    console.log("Tablas de multiplicar de numeros "+opcion);
-    let inicio = opcion === "pares" ? 2 : 1;
-    
+    console.log("Tablas de multiplicar de números " + opcion);
+    let inicio;
+    if (opcion === "pares") {
+        inicio = 2;
+    } else {
+        inicio = 1;
+    }
+
     for (let i = inicio; i <= 10; i += 2) {
-        console.log(`\nTabla del ${i}:`);
+        console.log("\nTabla del " + i + ":");
         for (let j = 1; j <= 10; j++) {
-            console.log(`${i} x ${j} = ${i * j}`);
+            console.log(i + " X " + j + " = " + (i * j));
         }
     }
 } else {
+
     console.log("Opción no válida. Por favor escribe 'pares' o 'impares'.");
 }
